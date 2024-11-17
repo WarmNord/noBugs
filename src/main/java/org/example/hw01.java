809aad30a2b3b299d513f4a569ab052bd4556e5c
funtikova <funtikova95@mail.ru>
2024-11-17 17:56:38 +0300

    Tasks hw 01

--- src/main/java/org/example/hw01.java
+++ src/main/java/org/example/hw01.java
@@ -32,5 +32,76 @@ public class hw01 {
     }
 
     //проверка знака числа
+    public String checkSignNumber(int num) {
+        if (num > 0) {
+            return "Positive";
+        } else if (num < 0) {
+            return "Negative";
+        } else {
+            return "Zero";
+        }
+    }
 
+    public boolean checkDivisionTwoThree(int number) {
+        return number % 2 == 0 || number % 3 == 0;
+    }
+
+    public String task9(int num1, int num2) {
+        if ((num1 + num2) % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+    public boolean task10(int number) {
+        return number % 4 == 0 && number % 6 == 0;
+    }
+
+    public String task11(int num1, int num2) {
+        int sum = num1 + num2;
+        if (sum % 2 == 0 && sum % 10 == 0 ) {
+            return  "Even and Divisible by 10";
+        } else if (sum % 2 == 0 && sum % 10 != 0 ){
+            return  "Even but not Divisible by 10";
+        } else {
+            return "Odd";
+        }
+    }
+
+    public int task12(int num1, int num2, int num3) {
+        return Math.max(Math.max(num1, num2), num3) ;
+    }
+
+    public boolean task13(int number) {
+        StringBuilder str = new StringBuilder();
+        String reverse = str.append(number).reverse().toString();
+        return Integer.parseInt(reverse) == number;
+    }
+
+    public Integer task14(int num1, int num2) {
+        if (num1 % 3 == 0 && num2 % 3 ==0) {
+            return num1 + num2;
+        } else if (num1 % 3 == 0 || num2 % 3 ==0) {
+            return num2 * 2;
+        } else {
+            return null;
+        }
+    }
+
+    public String task15(int num) {
+        if (num <= 12) {
+            return  "Child";
+        } else if (num >= 13 && num <= 17){
+            return  "Teenager";
+        } else if (num >= 18 && num <= 64){
+            return "Adult";
+        } else {
+            return "Senior";
+        }
+    }
+
+    public boolean task16(int num1, int num2) {
+        return num1 % num2 == 2;
+    }
 }
